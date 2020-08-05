44bfdf87fcd6fd8944bf0d336f25f30f4e04bfc2
Anvartdinov Timur <AnvartdinovTimurLinux@gmail.com>
2020-08-05 11:22:38 +0300

    Дерево интересов

---
+++ html-forms/interests/task.js
@@ -0,0 +1,47 @@
+'use strict';
+
+
+const checkedParent = (element) => {
+  const parent = element.closest('.interests_active')
+                        .closest('.interest');
+
+  if (parent) {
+    const childrens = [...parent.querySelectorAll('.interest__check')].slice(1);
+    const target = parent.querySelector('.interest__check');
+
+    if (childrens.every((children) => children.checked === true)) {
+      target.indeterminate = false;
+      target.checked = true;
+    } else if (childrens.every((children) => children.checked === false)) {
+      target.indeterminate = false;
+      target.checked = false;
+    } else {
+      target.indeterminate = true;
+    }
+
+    if(parent.closest('.interests_active')) {
+      checkedParent(target);
+    }
+  }
+};
+
+
+const interests = document.querySelector('.interests_main');
+interests.addEventListener('change', event => {
+  const target = event.target.closest('.interest');
+  const childrens = [...target.querySelectorAll('.interest__check')].slice(1);
+  const parent = target.querySelector('.interest__check');
+
+  childrens.forEach(children => {
+    if (parent.checked) {
+      children.checked = true;
+    } else {
+      children.checked = false;
+    }
+  });
+  // console.log(target)
+  checkedParent(target);
+});
+
+
+// children.indeterminate = true;
