e0def732e76341b0f84ca1990df8b11a181e3904
Anvartdinov Timur <AnvartdinovTimurLinux@gmail.com>
2020-08-07 17:36:01 +0300

    Корзина товаров

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,41 +1,83 @@
 'use strict';
 
-const cartProducts = document.querySelector('.cart__products');
 
-// управление колличеством товара в каталоге и добавление в корзину
+const cartProducts = document.querySelector('.cart__products');
 const products = document.querySelectorAll('.product');
+const cart = [];
+
+
+const addProductToCart = (productID, imgSrc, quantity) => {
+  const newProduct = document.createElement('div');
+  newProduct.className = 'cart__product';
+  newProduct.dataset.id = productID;
+  newProduct.innerHTML = `
+    <img class="cart__product-image" src="${imgSrc}">
+    <div class="cart__product-control">
+      <div class="cart__product-count">${quantity}</div>
+      <a href="#" class="cart__product-remove">&times;</a>
+    </div>`;
+  document.querySelector('.cart').classList.remove('cart-empty');
+
+  cartProducts.append(newProduct);
+  cart.push({
+    ID: productID,
+    img: imgSrc,
+    quantity: quantity,
+  });
+  localStorage.setItem('cart', JSON.stringify(cart))
+
+  
+  const removeBtn = newProduct.querySelector('.cart__product-remove');
+  removeBtn.addEventListener('click', () => {
+    event.preventDefault();
+    newProduct.remove();
+    cart.splice(cart.findIndex((element) => element.ID == productID), 1);
+    localStorage.setItem('cart', JSON.stringify(cart))
+    
+    if (cart.length === 0) {
+      document.querySelector('.cart').classList.add('cart-empty');
+    }
+  });
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const cartFromLS = JSON.parse(localStorage.getItem('cart'));
+  if (cartFromLS) {
+    for (let product of cartFromLS) {
+      addProductToCart(product.ID, product.img, product.quantity);
+    }
+  }
+});
+
+
 products.forEach((product) => {
-  const quantityValue = product.querySelector('.product__quantity-value');
+  const quantity = product.querySelector('.product__quantity-value');
   const quantityDec = product.querySelector('.product__quantity-control_dec');
   const quantityInc = product.querySelector('.product__quantity-control_inc');
+  const addBtn = product.querySelector('.product__add');
 
-  // уменьшение/увеличение количества товара
-  quantityDec.addEventListener('click', () => {
-    quantityValue.textContent = +quantityValue.textContent === 1
-      ? 1
-      : +quantityValue.textContent - 1;
-  });
   quantityInc.addEventListener('click', () => {
-    quantityValue.textContent = +quantityValue.textContent + 1;
+    quantity.textContent = +quantity.textContent + 1;
   });
-
-  // добавление товара в корзину
-  const productAdd = product.querySelector('.product__add');
-  productAdd.addEventListener('click', () => {
+  quantityDec.addEventListener('click', () => {
+    quantity.textContent = +quantity.textContent === 1
+      ? 1
+      : +quantity.textContent - 1;
+  });
+  
+  addBtn.addEventListener('click', () => {
     const productID = product.dataset.id;
-    if (Array.from(cartProducts.children).some(cartProduct => cartProduct.dataset.id == productID)) {
-      const currentCartproduct = cartProducts.querySelector(`[data-id="${productID}"]`);
-      currentCartproduct.children[1].innerText = +currentCartproduct.children[1].innerText + +quantityValue.textContent;
-    } else {
-      const newCartProduct = document.createElement('div');
-      newCartProduct.className = 'cart__product';
-      newCartProduct.dataset.id = productID;
-      newCartProduct.innerHTML = `
-        <img class="cart__product-image" src="${product.querySelector('img').src}">
-        <div class="cart__product-count">${quantityValue.textContent}</div>
-      `;
 
-      cartProducts.insertAdjacentElement('beforeEnd', newCartProduct);
+    if (Array.from(cartProducts.children).some((cartProduct) => cartProduct.dataset.id === productID)) {
+      const editedProduct = cartProducts.querySelector(`[data-id="${productID}"]`);
+      const productQuantity = editedProduct.querySelector('.cart__product-count');
+      productQuantity.textContent = +productQuantity.textContent + +quantity.textContent;
+
+      cart[cart.findIndex((element) => element.ID == productID)].quantity += +quantity.textContent;
+      localStorage.setItem('cart', JSON.stringify(cart))
+    } else {
+      addProductToCart(productID, product.querySelector('img').src, +quantity.textContent);
     }
   });
 });
